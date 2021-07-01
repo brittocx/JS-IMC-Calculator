543ef67a3ea3f50b96273abf39357061209fd9e4
brittocx <brittocx@gmail.com>
2021-07-01 20:39:22 -0300

    Just a simple JavaScript IMC Calculator

---
+++ imc.js
@@ -0,0 +1,33 @@
+const calcular = document.getElementById("calcular");
+
+function imc() {
+  const nome = document.getElementById("nome").value;
+  const altura = document.getElementById("altura").value;
+  const peso = document.getElementById("peso").value;
+  const resultado = document.getElementById("resultado");
+
+  if (nome !== "" && altura !== "" && peso !== "") {
+    const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+    let classificacao = " ";
+
+    if (valorIMC < 18.5) {
+      classificacao = "abaixo do peso.";
+    } else if (valorIMC < 25) {
+      classificacao = "com peso ideal. Parabéns!";
+    } else if (valorIMC < 30) {
+      classificacao = "levemente acima do peso.";
+    } else if (valorIMC < 35) {
+      classificacao = "obesidade grau I";
+    } else if (valorIMC < 40) {
+      classificacao = "obesidade grau II";
+    } else {
+      classificacao = "obesidade grau III. Cuidado!!";
+    }
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+  } else {
+    alert("Preencha todos os campos antes de calcular!!!");
+  }
+}
+
+calcular.addEventListener("click", imc);
